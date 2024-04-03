00f65032832d019435a40d3a3efc732b9fb36e0b
nullrun123 <165893187+nullrun123@users.noreply.github.com>
2024-04-03 16:10:48 +0700

    project Start

---
+++ Assets/Scenes/Script/Sellobj.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.UI;
+public class sellitem : MonoBehaviour
+{
+    private int score = 0;
+    public Text scoreText;
+    private void OnTriggerEnter(Collider target)
+    {
+
+        if (target.gameObject.tag.Equals("canPickUp"))
+        {
+            Debug.Log("ชน");
+            score += 10;
+            scoreText.text = "Score : " + score.ToString();
+            Destroy(target.gameObject);
+
+        }
+    }
+}
